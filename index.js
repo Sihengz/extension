c1a5d7c5fd2ecff296afe2fb4cf5f549421a617a
unknown <BT_3W2_07@BKTECH.LOCAL>
2024-11-21 15:32:33 -0500

    local storage

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ let inputEl = $("#input-el");
 let ulEl = $("#ul-el")
 // for (const website in myLeads) {
 //     console.log(myLeads[website])
-    
 // }
 // ulEl.each(function() {
 //     console.log($(this).text())
 // })
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+}
+console.log(leadsFromLocalStorage)
+for (let i = 0; i < myLeads.length; i++) {
+    const element = myLeads[i];
+    ulEl.append(`<li><a href="https://${element}">${element}</a></li>`)
+}
+
 $("#input-btn").click(function() {
     myLeads.push(inputEl.val())
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     ulEl.append(`<li><a href="https://${inputEl.val()}">${inputEl.val()}</a></li>`)
     inputEl.val("")
     console.log(myLeads)
